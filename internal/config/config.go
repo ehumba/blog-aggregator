6b4830791b3cd6edfa5392fc7ae51fe0546506c5
ehumba <behsukiat@gmail.com>
2025-07-13 13:16:30 +0800

    created Config struct and Read function

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,38 @@
 package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	DB_URL          string `json: db_url`
+	CurrentUserName string `json: current_user_name,omitempty`
+}
+
+func Read() (Config, error) {
+	// Get and read the gatorconfig file
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Print("no home directory")
+		return Config{}, err
+	}
+	configFile := homeDir + "/" + "gatorconfig.json"
+
+	data, err := os.ReadFile(configFile)
+	if err != nil {
+		return Config{}, fmt.Errorf("could not read gatorconfig: %v", err)
+	}
+
+	// Decode data into Config struct
+	var config Config
+	if err := json.Unmarshal(data, &config); err != nil {
+		return Config{}, fmt.Errorf("could not unmarshal data: %v", err)
+	}
+	return config, nil
+}
+
+func (c Config) SetUser(username string) {
+	//make this function
+}
