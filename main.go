ef7ffa91440e7e0c5d1a84395d957684bbbe1af3
ehumba <behsukiat@gmail.com>
2025-07-17 12:35:14 +0800

    set up cli in main()

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/ehumba/blog-aggregator/internal/config"
 )
 
@@ -14,5 +17,26 @@ func main() {
 	currentState := state{
 		cfg: &conf,
 	}
+
+	//Initialize the commands map
+	cmds := commands{
+		cmds: make(map[string]func(*state, command) error),
+	}
+
+	cmds.register("login", handlerLogin)
+
+	// Get command-line arguments passed by the user
+	argsWithProg := os.Args
+	argsWithoutProg := os.Args[1:]
+
+	if len(argsWithoutProg) < 2 {
+		fmt.Println("not enough arguments provided")
+		os.Exit(1)
+	}
+
+	inputCmdName := argsWithProg[1]
+	inputArgs := argsWithProg[2:]
+
+	cmds.run(&currentState, command{name: inputCmdName, args: inputArgs})
 
 }
